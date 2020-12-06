6bb95517c23edb8ead4c965d93a52a1626ded5c0
hatori9 <adriatorija@gmail.com>
2020-12-06 13:05:37 +0100

    Messages

--- Prac/src/Users.java
+++ Prac/src/Users.java
@@ -1,4 +1,40 @@
 
 public class Users {
+	private String userName;
+	private String name;
+	private int year;
+
+	public Users(String userName, String name, int year) {
+		super();
+		this.userName = userName;
+		this.name = name;
+		this.year = year;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	
 
 }
